18956df80057ea4f9cdf0fa7815236cda863727d
Camila Rosas <camila.rosas@ucb.edu.bo>
2024-08-06 10:44:50 -0400

    ultima modificacion del proyecto

--- modulo 6/main.js
+++ modulo 6/main.js
@@ -1,12 +1,12 @@
 //cambios realizados en el codigo
 
-// esta es una funcion
+// esta es una funcion que llama a otra funcion
 function outer() {
     let name = "outer";
     let str = inner();
     return str;
 }
-
+//esta es una funcion para decir hola 
 function inner() {
     let name = "inner";
     return "¡Hola!";
@@ -16,7 +16,6 @@ console.log("Antes de llamar a outer()");
 debugger;
 console.log(outer());
 console.log("Despues de llamar a outer()");
-
 
 //ejercicios de examen
 
